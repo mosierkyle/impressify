e9e70d63f0ab85330129a1bfb16f46f0cb0c989e
Kyle Mosier <mosierkyle@gmail.com>
2023-12-14 10:46:13 -0800

    Add fontAwesome svgs

--- src/components/generalTemplate.jsx
+++ src/components/generalTemplate.jsx
@@ -1,14 +1,32 @@
 import { useState } from 'react';
+import { Helmet } from 'react-helmet';
 import '../styles/generalTemplate.css';
 
 export default function GeneralTemplate({ formData }) {
   return (
     <div className="general-template">
+      <Helmet>
+        <script
+          src="https://kit.fontawesome.com/bdec03c25d.js"
+          crossorigin="anonymous"
+        ></script>
+      </Helmet>
       <h1 className="fullname"> Kyle Mosier{formData.fullName}</h1>
       <div className="info">
-        <p> mosierkyle@gmail.com{formData.email}</p>
-        <p> (360)597-8103 {formData.phone}</p>
-        <p> 5908 NW 38th ave #217{formData.address}</p>
+        <p>
+          {' '}
+          <i className="fa-solid fa-envelope svg"></i>
+          mosierkyle@gmail.com
+          {formData.email}
+        </p>
+        <p>
+          <i className="fa-solid fa-phone svg"></i>
+          (360)597-8103 {formData.phone}
+        </p>
+        <p>
+          <i className="fa-solid fa-location-dot svg"></i>
+          5908 NW 38th ave #217{formData.address}
+        </p>
       </div>
     </div>
   );
